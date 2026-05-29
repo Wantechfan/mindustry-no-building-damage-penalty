5680d9ce278d9e4132076561a630ae96359396d6
WantechFan <alifiandratsaqifp@gmail.com>
2026-05-30 00:42:35 +0700

    Al suspulo units

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 Events.on(ClientLoadEvent, e => { 
     //Lancer
     Blocks.lancer.shootType.buildingDamageMultiplier = 1;
+    //Arc
+    Blocks.arc.shootType.buildingDamageMultiplier = 1;
+    //Foreshadow
+    Blocks.foreshadow.ammoTypes.get(Items.surgeAlloy).buildingDamageMultiplier = 1;
     //Scathe
     Blocks.scathe.ammoTypes.get(Items.carbide).spawnUnit.weapons.get(0).bullet.buildingDamageMultiplier = 1;
     Blocks.scathe.ammoTypes.get(Items.carbide).spawnUnit.weapons.get(0).bullet.fragBullet.buildingDamageMultiplier = 1;
